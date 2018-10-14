2c4a0a0450129ee77137254cbaf74be3fcb70914
Francisco Hodge <hello@franciscohodge.com>
2018-10-14 16:09:53 -0400

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <div id="app">
+    <div class="img">
+      <img width="25%" src="./assets/logo.png">
+    </div>
+    <input
+      v-model="input"
+      class="input"
+      placeholder="Tap on the virtual keyboard to start"
+      readonly
+    />
+    <div class="simple-keyboard"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["input"],
+};
+</script>
+
+<style>
+app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+input {
+  width: 100%;
+  height: 100px;
+  padding: 20px;
+  font-size: 20px;
+  border: none;
+}
+
+.img {
+  text-align: center;
+}
+
+.img img {
+  width: 100px;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,54 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from "vue";
+import App from "./App";
+
+import Keyboard from "simple-keyboard";
+import "simple-keyboard/build/css/index.css";
+
+Vue.config.productionTip = false;
+
+/* eslint-disable no-new */
+let vueInstance = new Vue({
+  el: "#app",
+  components: { App },
+  template: `<App :input.sync="input" />`,
+  data: {
+    input: ""
+  },
+  methods: {
+    update(input) {
+      this.input = input;
+    }
+  }
+});
+
+let keyboard = new Keyboard({
+  onChange: input => onChange(input),
+  onKeyPress: button => onKeyPress(button)
+});
+
+console.log(keyboard);
+
+function onChange(input) {
+  vueInstance.update(input);
+  console.log("Input changed", input);
+}
+
+function onKeyPress(button) {
+  console.log("Button pressed", button);
+
+  /**
+   * If you want to handle the shift and caps lock buttons
+   */
+  if (button === "{shift}" || button === "{lock}") handleShift();
+}
+
+function handleShift() {
+  let currentLayout = keyboard.options.layoutName;
+  let shiftToggle = currentLayout === "default" ? "shift" : "default";
+
+  keyboard.setOptions({
+    layoutName: shiftToggle
+  });
+}
