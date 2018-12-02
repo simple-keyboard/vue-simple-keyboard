f56838f2650dcfd3e645eb93db0248c1816a971b
Francisco Hodge <hello@franciscohodge.com>
2018-12-02 17:25:43 -0500

    full-keyboard

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,19 @@
       class="input"
       placeholder="Tap on the virtual keyboard to start"
     />
-    <div class="simple-keyboard"></div>
+    <div class="keyboardContainer">
+      <div class="simple-keyboard-main"></div>
+
+      <div class="controlArrows">
+        <div class="simple-keyboard-control"></div>
+        <div class="simple-keyboard-arrows"></div>
+      </div>
+
+      <div class="numPad">
+        <div class="simple-keyboard-numpad"></div>
+        <div class="simple-keyboard-numpadEnd"></div>
+      </div>
+    </div>
   </div>
 </template>
 
--- src/main.js
+++ src/main.js
@@ -10,6 +10,7 @@ import "./index.css";
 Vue.config.productionTip = false;
 
 let keyboard;
+let commonKeyboardOptions;
 
 /* eslint-disable no-new */
 let vueInstance = new Vue({
@@ -30,15 +31,114 @@ let vueInstance = new Vue({
   }
 });
 
-keyboard = new Keyboard({
+commonKeyboardOptions = {
   onChange: input => onChange(input),
-  onKeyPress: button => onKeyPress(button)
+  onKeyPress: button => onKeyPress(button),
+  theme: "simple-keyboard hg-theme-default hg-layout-default",
+  physicalKeyboardHighlight: true,
+  syncInstanceInputs: true,
+  mergeDisplay: true,
+  debug: true
+};
+
+keyboard = new Keyboard(".simple-keyboard-main", {
+  ...commonKeyboardOptions,
+  /**
+   * Layout by:
+   * Sterling Butters (https://github.com/SterlingButters)
+   */
+  layout: {
+    default: [
+      "{escape} {f1} {f2} {f3} {f4} {f5} {f6} {f7} {f8} {f9} {f10} {f11} {f12}",
+      "` 1 2 3 4 5 6 7 8 9 0 - = {backspace}",
+      "{tab} q w e r t y u i o p [ ] \\",
+      "{capslock} a s d f g h j k l ; ' {enter}",
+      "{shiftleft} z x c v b n m , . / {shiftright}",
+      "{controlleft} {altleft} {metaleft} {space} {metaright} {altright}"
+    ],
+    shift: [
+      "{escape} {f1} {f2} {f3} {f4} {f5} {f6} {f7} {f8} {f9} {f10} {f11} {f12}",
+      "~ ! @ # $ % ^ & * ( ) _ + {backspace}",
+      "{tab} Q W E R T Y U I O P { } |",
+      '{capslock} A S D F G H J K L : " {enter}',
+      "{shiftleft} Z X C V B N M < > ? {shiftright}",
+      "{controlleft} {altleft} {metaleft} {space} {metaright} {altright}"
+    ]
+  },
+  display: {
+    "{escape}": "esc ⎋",
+    "{tab}": "tab ⇥",
+    "{backspace}": "backspace ⌫",
+    "{enter}": "enter ↵",
+    "{capslock}": "caps lock ⇪",
+    "{shiftleft}": "shift ⇧",
+    "{shiftright}": "shift ⇧",
+    "{controlleft}": "ctrl ⌃",
+    "{controlright}": "ctrl ⌃",
+    "{altleft}": "alt ⌥",
+    "{altright}": "alt ⌥",
+    "{metaleft}": "cmd ⌘",
+    "{metaright}": "cmd ⌘"
+  }
 });
 
-console.log(keyboard);
+let keyboardControlPad = new Keyboard(".simple-keyboard-control", {
+  ...commonKeyboardOptions,
+  layout: {
+    default: [
+      "{prtscr} {scrolllock} {pause}",
+      "{insert} {home} {pageup}",
+      "{delete} {end} {pagedown}"
+    ]
+  }
+});
+
+let keyboardArrows = new Keyboard(".simple-keyboard-arrows", {
+  ...commonKeyboardOptions,
+  layout: {
+    default: ["{arrowup}", "{arrowleft} {arrowdown} {arrowright}"]
+  }
+});
+
+let keyboardNumPad = new Keyboard(".simple-keyboard-numpad", {
+  ...commonKeyboardOptions,
+  layout: {
+    default: [
+      "{numlock} {numpaddivide} {numpadmultiply}",
+      "{numpad7} {numpad8} {numpad9}",
+      "{numpad4} {numpad5} {numpad6}",
+      "{numpad1} {numpad2} {numpad3}",
+      "{numpad0} {numpaddecimal}"
+    ]
+  }
+});
+
+let keyboardNumPadEnd = new Keyboard(".simple-keyboard-numpadEnd", {
+  ...commonKeyboardOptions,
+  layout: {
+    default: ["{numpadsubtract}", "{numpadadd}", "{numpadenter}"]
+  }
+});
+
+/**
+ * Physical Keyboard support
+ * Whenever the input is changed with the keyboard, updating SimpleKeyboard's internal input
+ */
+/*document.addEventListener("keydown", event => {
+  // Disabling keyboard input, as some keys (like F5) make the browser lose focus.
+  // If you're like to re-enable it, comment the next line and uncomment the following ones
+  event.preventDefault();
+});*/
+
+document.querySelector(".input").addEventListener("input", event => {
+  let input = document.querySelector(".input").value;
+  keyboard.setInput(input);
+});
 
 function onChange(input) {
-  vueInstance.update(input);
+  document.querySelector(".input").value = input;
+  keyboard.setInput(input);
+
   console.log("Input changed", input);
 }
 
@@ -48,7 +148,13 @@ function onKeyPress(button) {
   /**
    * If you want to handle the shift and caps lock buttons
    */
-  if (button === "{shift}" || button === "{lock}") handleShift();
+  if (
+    button === "{shift}" ||
+    button === "{shiftleft}" ||
+    button === "{shiftright}" ||
+    button === "{capslock}"
+  )
+    handleShift();
 }
 
 function handleShift() {
