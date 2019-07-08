947f827f3e8b196a4de38e89e67540bc6f346551
Francisco Hodge <hello@franciscohodge.com>
2019-07-07 23:34:32 -0400

    Dark theme

--- src/main.js
+++ src/main.js
@@ -1,62 +1,8 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from "vue";
-import App from "./App";
-
-import Keyboard from "simple-keyboard";
-import "simple-keyboard/build/css/index.css";
-import "./index.css";
+import App from "./App.vue";
 
 Vue.config.productionTip = false;
 
-let keyboard;
-
-/* eslint-disable no-new */
-let vueInstance = new Vue({
-  el: "#app",
-  components: { App },
-  template: `<App :input.sync="input" :onInputChange="onInputChange"/>`,
-  data: {
-    input: ""
-  },
-  methods: {
-    update(input) {
-      this.input = input;
-    },
-    onInputChange(e) {
-      let input = e.target.value;
-      keyboard.setInput(input);
-    }
-  }
-});
-
-keyboard = new Keyboard({
-  onChange: input => onChange(input),
-  onKeyPress: button => onKeyPress(button),
-  theme: "hg-theme-default myTheme1"
-});
-
-console.log(keyboard);
-
-function onChange(input) {
-  vueInstance.update(input);
-  console.log("Input changed", input);
-}
-
-function onKeyPress(button) {
-  console.log("Button pressed", button);
-
-  /**
-   * If you want to handle the shift and caps lock buttons
-   */
-  if (button === "{shift}" || button === "{lock}") handleShift();
-}
-
-function handleShift() {
-  let currentLayout = keyboard.options.layoutName;
-  let shiftToggle = currentLayout === "default" ? "shift" : "default";
-
-  keyboard.setOptions({
-    layoutName: shiftToggle
-  });
-}
+new Vue({
+  render: h => h(App)
+}).$mount("#app");
