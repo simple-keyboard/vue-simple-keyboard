12e4268d5e9683e72c9fcaf494213be749598a44
Francisco Hodge <hello@franciscohodge.com>
2019-07-07 23:17:08 -0400

    Component demo

--- src/App.vue
+++ src/App.vue
@@ -2,28 +2,44 @@
   <div id="app">
     <input
       :value="input"
-      @input="onInputChange"
       class="input"
+      @input="onInputChange"
       placeholder="Tap on the virtual keyboard to start"
-    />
-    <div class="simple-keyboard"></div>
+    >
+    <SimpleKeyboard @onChange="onChange" @onKeyPress="onKeyPress" :input="input"/>
   </div>
 </template>
 
 <script>
+import SimpleKeyboard from "./SimpleKeyboard";
+import "./App.css";
+
 export default {
-  props: ["input", "onInputChange"]
+  name: "App",
+  components: {
+    SimpleKeyboard
+  },
+  data: () => ({
+    input: ""
+  }),
+  methods: {
+    onChange(input) {
+      this.input = input;
+    },
+    onKeyPress(button) {
+      console.log("button", button);
+    },
+    onInputChange(input) {
+      this.input = input.target.value;
+    }
+  }
 };
 </script>
 
 <style>
-app {
+#app {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
-
 </style>
